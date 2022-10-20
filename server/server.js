4b8f4ddf9953e26cf605afccbba2b7d403e246c1
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-10-19 22:39:53 -0300

    pages

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const cors = require('cors')
+const path = require('path')
 const app = express()
 const Route = require('./route/routeBiga.js')
 const route = require('./route/routeMedia.js')
@@ -8,9 +9,20 @@ const port = process.env.PORT || 2000
 app.use(express.json())
 app.use(express.urlencoded({extended: false}))
 app.use(cors())
+app.use(express.static(path.join(__dirname,'public')))
+console.log(path.join(__dirname, 'public'))
+
+app.set('view engine', 'ejs')
+app.set('views', './views')
 
 app.use('/', Route)
 app.use('/', route)
+
+app.get('/', (req, res)=>{
+    res.render('index.ejs')
+    
+})
+
 
 app.listen(port, ()=>{
     console.log('rodando no link: https://ceara-cientifico.herokuapp.com/')
