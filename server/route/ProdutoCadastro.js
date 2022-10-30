704f2b09b23b1243a2ac227cc0fd0720f2a64f24
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-10-30 18:13:49 -0300

    API AND SCREENS FINISHED

--- server/route/ProdutoCadastro.js
+++ server/route/ProdutoCadastro.js
@@ -16,6 +16,7 @@ RoutePost.post('/pages/atualizar', async (req, res)=>{
         const obj = {}
         const objVerification = {
             tipo: req.body.tipof,
+            marca: req.body.marcaf,
             estado: req.body.estadof,
             classificacao: req.body.classificacaof,
             next: req.body.next
@@ -37,10 +38,6 @@ RoutePost.post('/pages/atualizar', async (req, res)=>{
     
         if(req.body.tipo !== 'nenhum'){
             obj.tipo =  req.body.tipo
-        }
-
-        if(req.body.marcaf !== 'nenhum'){
-            objVerification.marca = req.body.marcaf
         }
     
         if(req.body.marca !== 'nenhum'){
