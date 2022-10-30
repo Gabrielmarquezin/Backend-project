3029fc5afa9010ce2003a38d66e62ce3a42f9e77
Gabrielmarquezin <gabrielmarquesaraujs@gmail.com>
2022-10-30 18:31:51 -0300

    API AND SCREENS FINISHED

--- server/route/ProdutoCadastro.js
+++ server/route/ProdutoCadastro.js
@@ -29,7 +29,7 @@ RoutePost.post('/pages/atualizar', async (req, res)=>{
         }
 
         if(objVerification.marca == 'nenhum'){
-            objVerification.marca = false
+            objVerification.marca = 'not'
         }
 
         if(tamanho < 1000 && tamanho !== 0){
