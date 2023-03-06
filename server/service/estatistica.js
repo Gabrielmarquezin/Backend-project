b3b6ef791ecbf51e86d70a32a7cd1985c49f5cd9
Gabrielmarquezin <Gabrielmarquesaraujs@gmail.com>
2023-03-05 22:27:59 -0300

    moda finished

--- server/service/estatistica.js
+++ server/service/estatistica.js
@@ -1,7 +1,7 @@
 
 function calStatistic(arrayModify){
-    console.log(arrayModify)
-    
+    let moda;
+
     let Media = arrayModify.reduce((previusValue, currentValue) =>{
         return previusValue+currentValue/arrayModify.length
     }, 0)
@@ -14,8 +14,46 @@ function calStatistic(arrayModify){
 
     let DesvioPadrao = parseFloat(Math.sqrt(Variancia).toFixed(2))
 
-    return {Media: Media, Variancia: Variancia, DesvioPadrao: DesvioPadrao}
+    if(Media == arrayModify[0]){
+        moda = Media
+    }else{
+        moda =  Moda(arrayModify)
+    }
+
+    return {Media: Media, Variancia: Variancia, DesvioPadrao: DesvioPadrao, Moda: moda}
 }
 
+
+function Moda(arr){
+    let contValues = Object.create(null)
+    let moda;
+
+    for(let values of arr){
+        if(!contValues[values]){
+            contValues[values] = 1
+        }else{
+            contValues[values]++
+        }
+    }
+
+    let objValues = Object.entries(contValues).map(([value, cont])=>{
+        return {
+            value: parseFloat(value),
+            cont: cont
+        }
+    })
+
+    let maxValue = objValues.reduce((a, b)=>{
+        return a.cont > b.cont ? a:b
+    })
+   
+    if(maxValue.cont == 1){
+        moda = "Nao identificado"
+    }else{
+        moda = parseFloat(maxValue.value.toFixed(2))
+    }
+    
+    return moda;
+}
 
 module.exports = calStatistic
